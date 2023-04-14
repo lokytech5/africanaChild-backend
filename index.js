0b2f8c7f51aeaab24918deb0e11adedf108d2eab
lokytech <lokytech5@gmail.com>
2023-04-14 23:28:31 +0100

    "cors configuration for react app"

--- index.js
+++ index.js
@@ -17,7 +17,24 @@ app.use(express.json());
 
 
 //*using Cors
-app.use(cors());
+const allowedOrigins = [
+    'http://localhost:3000', // Replace 3000 with the port you are using for your React app during development
+    'https://your-deployed-react-app.com' // Replace with the URL of your deployed React app
+];
+
+app.use(cors({
+    origin: (origin, callback) => {
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    },
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'x-auth-token']
+}));
+
 
 
 require('./start/routes')(app);
